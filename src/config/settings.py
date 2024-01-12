bd515e499d1c8878a3e074a228a2ca7ecb55e89f
alex22201 <tyutyunnikaliosha@gmail.com>
2024-01-12 14:10:43 +0200

    fix bug with root and staticfiles

--- src/config/settings.py
+++ src/config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'pw+*a^45y7s_f(im29%4ot2222m1h7zed4w+$03_trf4)0)!l!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Cors
 #
